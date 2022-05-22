e35c450c25b245f1b52b093cce734eea91f0ff88
Romulo Lima <romulosilva-07@hotmail.com>
2022-05-22 00:27:59 -0300

    inserted entity validation with fluentValidation and added behaviors for properties

---
+++ Manager.Domain/Entities/User.cs
@@ -0,0 +1,60 @@
+﻿using Manager.Domain.Validators;
+using System;
+using System.Collections.Generic;
+
+namespace Manager.Domain.Entities
+{
+    public class User : Base
+    {
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            _errors = new List<string>();
+        }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        //EF
+        protected User(){}
+
+        //Comportamento
+        public void ChangeName(string name)
+        {
+            Name = name;
+            Validate();
+        }
+
+        public void ChangePassword(string password)
+        {
+            Password = password;
+            Validate();
+        }
+
+        public void ChangeEmail(string email)
+        {
+            Email = email;
+            Validate();
+        }
+
+        //Autovalida
+        public override bool Validate()
+        {
+            var validator = new UserValidator();
+            var validation = validator.Validate(this);
+
+            if (!validation.IsValid)
+            {
+                foreach (var error in validation.Errors)
+                    _errors.Add(error.ErrorMessage);
+
+                throw new Exception("Alguns campos estão inválidos, por favor corrija-os" + _errors[0]);
+            }
+
+            return true;
+        }
+    }
+}
